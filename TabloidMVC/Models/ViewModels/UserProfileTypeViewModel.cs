6409c219c222f831ba89c44fc78abfccf61216c8
mnmccoy95 <mnmccoy95@gmail.com>
2021-01-05 14:57:46 -0600

    added ability to change user types

---
+++ TabloidMVC/Models/ViewModels/UserProfileTypeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TabloidMVC.Models.ViewModels
+{
+    public class UserProfileTypeViewModel
+    {
+        public UserProfile user { get; set; }
+        public List<UserType> type { get; set; }
+    }
+}
