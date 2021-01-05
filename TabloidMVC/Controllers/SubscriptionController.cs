951a32094cc20b95e62c07adf6700cbe5d685529
Travis Pinson <travispinson@gmail.com>
2021-01-05 15:46:03 -0600

    Groundwork and pathing for subscriptions. Object created. Needs repo function to add for completion

---
+++ TabloidMVC/Controllers/SubscriptionController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Controllers
+{
+    public class SubscriptionController : Controller
+    {
+        // GET: SubscriptionController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SubscriptionController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SubscriptionController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SubscriptionController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SubscriptionController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SubscriptionController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SubscriptionController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SubscriptionController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Subscribe(int providerId, int postId)
+        {
+            Subscription subscription = new Subscription();
+
+            subscription.ProviderUserProfileId = providerId;
+            subscription.SubscriberUserProfileId = GetCurrentUserId();
+            subscription.BeginDateTime = DateTime.Now;
+
+            string postString = postId.ToString();
+
+            return RedirectToAction("Details", "Post", new { id = postId });
+
+
+        }
+
+        private int GetCurrentUserId()
+        {
+            string id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            return int.Parse(id);
+        }
+    }
+}
---
+++ TabloidMVC/Repositories/SubscriptionRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+
+
+
+
+namespace TabloidMVC.Repositories
+{
+    public class SubscriptionRepository : BaseRepository
+    {
+        public SubscriptionRepository(IConfiguration config) : base(config) { }
+
+        
+
+        
+    }
+}
